73f9b3232615dab56a958c4a8ec19fd02d8063c5
yustin olarte perez <yustinolarte@gmail.com>
2025-11-29 01:20:27 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
